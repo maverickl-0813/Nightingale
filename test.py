1e2bb07c3ebb9a4edeb8fca0db286c80da4ba8ac
maverick <maverick.liou@gmail.com>
2023-12-15 01:27:50 +0800

    File rename.

--- test.py
+++ test.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     # runner.update_all()
     # runner.update_pharmacy()
     runner.update_medical_centers()
-    runner.update_regional_hospital()
-    runner.update_district_hospital()
+    # runner.update_regional_hospital()
+    # runner.update_district_hospital()
