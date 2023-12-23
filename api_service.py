287a711b02217812e10a6f4b62780266ef019981
maverick <maverick.liou@gmail.com>
2023-12-23 11:13:55 +0800

    Add new service path for division search.

--- api_service.py
+++ api_service.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_restful import Api
 from medical_site_data_service import GetSiteBasicData
 from medical_site_data_service import GetSiteCountByType
-from medical_site_data_service import ListMedicalSite
+from medical_site_data_service import GetMedicalSiteList
 from medical_site_data_service import GetSiteWorkingHours
+from medical_site_data_service import GetSiteListByDivision
 
 
 if __name__ == '__main__':
@@ -11,6 +12,7 @@ if __name__ == '__main__':
     api = Api(app)
     api.add_resource(GetSiteBasicData, "/site_basic_data")
     api.add_resource(GetSiteCountByType, "/site_count")
-    api.add_resource(ListMedicalSite, "/list_sites")
+    api.add_resource(GetMedicalSiteList, "/list_sites")
     api.add_resource(GetSiteWorkingHours, "/working_hours")
+    api.add_resource(GetSiteListByDivision, "/search_site_by_division")
     app.run(host='0.0.0.0', port=6400, ssl_context="adhoc", debug=True)
--- medical_site_data_service.py
+++ medical_site_data_service.py
@@ -16,12 +16,18 @@ class BaseClass(Resource):
     def _convert_mongodb_output_to_json(data):
         return json.loads(json.dumps(data, default=json_util.default))
 
-    def _list_site_by_type(self, site_type):
+    @staticmethod
+    def _validate_site_type(site_type):
+        if site_type not in supported_medical_site:
+            return {'message': f"Not supported medical site type: {site_type}"}, 400
+
+    def _list_sites_by_type(self, site_type):
         results = list()
         query_results_bson = self.db_controller.query_multi_in_collection(resource=site_type)
-        for items in query_results_bson:
-            items = self._convert_mongodb_output_to_json(items)
-            results.append(items)
+        for item in query_results_bson:
+            item = self._convert_mongodb_output_to_json(item)
+            del item["_id"]
+            results.append(item)
         return results
 
     def _query_site_type_by_id(self, site_type, site_id):
@@ -30,7 +36,24 @@ class BaseClass(Resource):
             query_filter = {'site_id': site_id}
             query_result_bson = self.db_controller.query_one_in_collection(resource=site_type, query_filter=query_filter)
             query_result = self._convert_mongodb_output_to_json(query_result_bson)
+            del query_result["_id"]
         return query_result
+
+    def _query_site_list_by_division(self, site_type, site_division):
+        results = list()
+        if site_division:
+            if len(site_division) > 3:
+                division_lv1 = site_division[:3]
+                division_lv2 = site_division[3:]
+                query_filter = {'site_region_lv1': division_lv1, 'site_region_lv2': division_lv2}
+            else:
+                query_filter = {'site_region_lv1': site_division}
+            query_result_bson = self.db_controller.query_multi_in_collection(resource=site_type, query_filter=query_filter)
+            for item in query_result_bson:
+                item = self._convert_mongodb_output_to_json(item)
+                del item["_id"]
+                results.append(item)
+        return results
 
     def _count_site_by_type(self, site_type):
         count = self.db_controller.count_in_collection(resource=site_type)
@@ -42,15 +65,14 @@ class GetSiteBasicData(BaseClass):
         super().__init__()
 
     def get(self):
-        site_type = request.args.get("site_type")
-        site_id = request.args.get("site_id")
-        if site_type in supported_medical_site:
-            query_result = self._query_site_type_by_id(site_type=site_type, site_id=site_id)
-            for key in ["_id", "site_region_lv1", "site_region_lv2", "site_service_list", "site_function_list", "site_working_hours"]:
-                del query_result[key]
-            return {'message': 'success', 'data': query_result}, 200
-        else:
-            return {'message': f"Not supported medical site type: {site_type}"}, 400
+        site_type = request.args.get("type")
+        site_id = request.args.get("id")
+        self._validate_site_type(site_type)
+        query_result = self._query_site_type_by_id(site_type=site_type, site_id=site_id)
+        for key in ["site_region_lv1", "site_region_lv2", "site_service_list", "site_function_list",
+                    "site_working_hours"]:
+            del query_result[key]
+        return {'message': 'success', 'data': query_result}, 200
 
 
 class GetSiteWorkingHours(BaseClass):
@@ -58,28 +80,24 @@ class GetSiteWorkingHours(BaseClass):
         super().__init__()
 
     def get(self):
-        site_type = request.args.get("site_type")
-        site_id = request.args.get("site_id")
-        if site_type in supported_medical_site:
-            query_result = self._query_site_type_by_id(site_type=site_type, site_id=site_id)
-            result = query_result.pop("site_working_hours")
-            return {'message': 'success', 'data': result}, 200
-        else:
-            return {'message': f"Not supported medical site type: {site_type}"}, 400
+        site_type = request.args.get("type")
+        site_id = request.args.get("id")
+        self._validate_site_type(site_type)
+        query_result = self._query_site_type_by_id(site_type=site_type, site_id=site_id)
+        result = query_result.pop("site_working_hours")
+        return {'message': 'success', 'data': result}, 200
 
 
-class ListMedicalSite(BaseClass):
+class GetMedicalSiteList(BaseClass):
     def __init__(self):
         super().__init__()
 
     def get(self):
-        site_type = request.args.get("site_type")
-        if site_type in supported_medical_site:
-            query_result = self._list_site_by_type(site_type=site_type)
-            site_count = self._count_site_by_type(site_type=site_type)
-            return {'message': 'success', 'count': site_count, 'data': query_result}, 200
-        else:
-            return {'message': f"Not supported medical site type: {site_type}"}, 400
+        site_type = request.args.get("type")
+        self._validate_site_type(site_type)
+        query_result = self._list_sites_by_type(site_type=site_type)
+        site_count = self._count_site_by_type(site_type=site_type)
+        return {'message': 'success', 'count': site_count, 'data': query_result}, 200
 
 
 class GetSiteCountByType(BaseClass):
@@ -87,9 +105,21 @@ class GetSiteCountByType(BaseClass):
         super().__init__()
 
     def get(self):
-        site_type = request.args.get("site_type")
-        if site_type in supported_medical_site:
-            site_count = self._count_site_by_type(site_type=site_type)
-            return {'message': 'success', 'count': site_count}, 200
-        else:
-            return {'message': f"Not supported medical site type: {site_type}"}, 400
+        site_type = request.args.get("type")
+        self._validate_site_type(site_type)
+        site_count = self._count_site_by_type(site_type=site_type)
+        return {'message': 'success', 'count': site_count}, 200
+
+
+class GetSiteListByDivision(BaseClass):
+
+    def __init__(self):
+        super().__init__()
+
+    def get(self):
+        site_type = request.args.get("type")
+        site_division = request.args.get("division")
+        self._validate_site_type(site_type)
+        query_result = self._query_site_list_by_division(site_type=site_type, site_division=site_division)
+        count = len(query_result)
+        return {'message': 'success', 'count': count, 'data': query_result}
