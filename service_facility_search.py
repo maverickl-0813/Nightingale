ad1f6642368f5973cf9cbfe8c90e240577cd347f
maverick <maverick.liou@gmail.com>
2023-12-27 22:09:06 +0800

    Add "keyword" for facility name search and adjust the required field check.

--- service_facility_search.py
+++ service_facility_search.py
@@ -10,12 +10,12 @@ class SearchSites(BaseClass):
         super().__init__()
         self.supported_medical_site = supported_medical_site
 
-    def _query_site_list_by_division_and_function_list(self, site_type, site_division, site_function_list):
+    def _query_sites(self, site_type, site_division, site_function_list, site_name_kw):
         results = list()
         query_filter = dict()
-        site_division = self._replace_tai(site_division)
 
         if site_division:
+            site_division = self._replace_tai(site_division)
             if (dv_tuple := self._site_division_preprocess(site_division)) is not None:
                 query_filter['site_region_lv1'] = dv_tuple[0]
                 if dv_tuple[1]:
@@ -26,6 +26,9 @@ class SearchSites(BaseClass):
         if site_function_list:
             query_filter['site_function_list'] = {'$all': site_function_list}
 
+        if site_name_kw:
+            query_filter['site_name'] = {'$regex': site_name_kw}
+
         # query database
         query_results_bson = self.db_controller.query_multi_in_collection(resource=site_type, query_filter=query_filter)
         for item in query_results_bson:
@@ -35,30 +38,29 @@ class SearchSites(BaseClass):
         return results
 
     def post(self):
-        request_data = request.json  # Get JSON data from the request body
+        request_data = request.json if request.json else {}  # Get JSON data from the request body
 
-        site_type_list = request_data.get("type") if request_data.get("type") else None
-        site_division = request_data.get("division") if request_data.get("division") else None
-        site_function_list = request_data.get("function") if request_data.get("function") else None
+        site_type_list = request_data.get("type")
+        site_division = request_data.get("division")
+        site_function_list = request_data.get("function")
+        site_name_keyword = request_data.get("keyword")
 
-        if not self._is_required_args_exists([site_type_list, site_division, site_function_list]):
+        if not self._is_required_args_exists([site_type_list]):     # Only site type ("type") is mandatory.
             return self._return_error_status(status_code=400,
-                                             message=f"Query criteria incomplete.")
+                                             message=f"Query criteria incomplete, provide 'type' with array. "
+                                                     f"Supported facility types: {self.supported_medical_site}")
 
         for site_type in site_type_list:
             if not self._is_site_type_valid(site_type):
                 return self._return_error_status(status_code=400,
                                                  message=f"Not supported medical site type: {site_type}")
 
-        if len(site_function_list) == 1 and site_function_list[0] == '':
-            return self._return_error_status(status_code=400,
-                                             message=f"Function is not provided with \"function\" argument properly.")
-
         query_result = list()
         for site_type in site_type_list:
-            query_type_result = self._query_site_list_by_division_and_function_list(site_type=site_type,
-                                                                                    site_division=site_division,
-                                                                                    site_function_list=site_function_list)
+            query_type_result = self._query_sites(site_type=site_type,
+                                                  site_division=site_division,
+                                                  site_function_list=site_function_list,
+                                                  site_name_kw=site_name_keyword)
             query_result += query_type_result
 
         count = len(query_result)
