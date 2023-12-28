7aef24e9bfa7581276db7dce90dab39ad40d7202
maverick <maverick.liou@gmail.com>
2023-12-28 22:41:34 +0800

    Fix sync script to stable.

--- sync_nhi_data.py
+++ sync_nhi_data.py
@@ -1,11 +1,13 @@
 import logging
-
+import datetime
+from pathlib import Path
 from DataMaintenance.fetch_medical_site import FetchMedicalSites
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
+# timestamp_file = 'sync.timestamp'
 
-if __name__ == '__main__':
 
+def run_sync():
     runner = FetchMedicalSites()
     # runner.update_all()
     runner.update_medical_centers()
@@ -13,3 +15,14 @@ if __name__ == '__main__':
     runner.update_district_hospital()
     runner.update_small_clinic()
     # runner.update_pharmacy()
+
+    # Write timestamp to file.
+    # with open(timestamp_file, 'w', encoding='utf-8') as f:
+    #     f.write(f"{datetime.datetime.now()}")
+
+
+if __name__ == '__main__':
+    # timestamp = Path(timestamp_file)
+    # if not timestamp:
+    #     run_sync()
+    run_sync()
