7d68b5652ec39e5ebd7ecb4264e11c87e3e39555
maverick <maverick.liou@gmail.com>
2024-01-07 17:11:07 +0800

    fix: clean things up.

--- sync_nhi_data.py
+++ sync_nhi_data.py
@@ -1,28 +1,13 @@
 import logging
-import datetime
-from pathlib import Path
 from DataMaintenance.fetch_medical_site import FetchMedicalSites
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
-# timestamp_file = 'sync.timestamp'
 
 
 def run_sync():
     runner = FetchMedicalSites()
-    # runner.update_all()
-    runner.update_medical_centers()
-    runner.update_regional_hospital()
-    runner.update_district_hospital()
-    runner.update_small_clinic()
-    # runner.update_pharmacy()
-
-    # Write timestamp to file.
-    # with open(timestamp_file, 'w', encoding='utf-8') as f:
-    #     f.write(f"{datetime.datetime.now()}")
+    runner.update_all()
 
 
 if __name__ == '__main__':
-    # timestamp = Path(timestamp_file)
-    # if not timestamp:
-    #     run_sync()
     run_sync()
