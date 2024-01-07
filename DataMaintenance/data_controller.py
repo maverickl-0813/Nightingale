98c65571268484cc70fa33afc854cf62af7aaa6e
maverick <maverick.liou@gmail.com>
2024-01-07 17:12:49 +0800

    feature: change login mechanism from certificate to user/pass.

--- DataMaintenance/data_controller.py
+++ DataMaintenance/data_controller.py
@@ -6,6 +6,8 @@ class DataController:
 
     def __init__(self):
         self.db_name = "med_data_db"
+        self.mongodb_user = os.environ['MONGODB_USER']
+        self.mongodb_pass = os.environ['MONGODB_PASS']
         self.collection_name = None
         self.db_client = None
         self.db_inst = None
@@ -14,10 +16,12 @@ class DataController:
         self.default_projection = {'_id': 0}
 
     def _init_db_conn(self):
-        uri = "mongodb+srv://nightingale-db.wgbwctt.mongodb.net/"
-        cert_file = f"{os.path.dirname(os.path.abspath(__file__))}/cert_file/X509-cert-1429016553120893234.pem"
-        self.db_client = pymongo.MongoClient(uri, tls=True, authMechanism="MONGODB-X509",
-                                             authSource="$external", tlsCertificateKeyFile=cert_file)
+        # uri = "mongodb+srv://nightingale-db.wgbwctt.mongodb.net/"
+        # cert_file = f"{os.path.dirname(os.path.abspath(__file__))}/cert_file/X509-cert-1429016553120893234.pem"
+        # self.db_client = pymongo.MongoClient(uri, tls=True, authMechanism="MONGODB-X509",
+        #                                      authSource="$external", tlsCertificateKeyFile=cert_file)
+        uri = f"mongodb+srv://{self.mongodb_user}:{self.mongodb_pass}@nightingale-db.wgbwctt.mongodb.net/"
+        self.db_client = pymongo.MongoClient(uri)
         self.db_inst = self.db_client[self.db_name]
 
     def _set_collection(self):
